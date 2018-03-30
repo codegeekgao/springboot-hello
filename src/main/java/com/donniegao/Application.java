ff940746aabdcb1f0b160aa2041f9fbdf4bff4ff
codegeekgao <codegeek_mrgao@163.com>
2018-03-30 13:34:24 +0800

    springBoot提交

---
+++ src/main/java/com/donniegao/Application.java
@@ -0,0 +1,36 @@
+/**
+ * Corearchi.com Inc.
+ * Copyright (c) 2017-2018 All Rights Reserved.
+ */
+package com.donniegao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * springBoot入门程序
+ *
+ * @author gaoxinpeng
+ * @version Id: Application.java, v 0.1 2018/3/28 上午9:31 gaoxinpeng Exp $$
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+    /**
+     * say hello
+     *
+     * @return 返回结果
+     */
+    @RequestMapping("/")
+    public String sayHello() {
+        return "hello, SpringBoot !";
+    }
+
+    public static void main(String[] args) {
+        // 运行SpringBoot
+        SpringApplication.run(Application.class,args);
+    }
+}
