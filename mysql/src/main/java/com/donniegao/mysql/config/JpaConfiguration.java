606dea9b3e8acbd46a087d185e5ad9a08cc116a1
codegeekgao <codegeek_mrgao@163.com>
2018-04-04 01:09:15 +0800

    PersistenceExceptionTranslationPostProcessor 的作用

--- mysql/src/main/java/com/donniegao/mysql/config/JpaConfiguration.java
+++ mysql/src/main/java/com/donniegao/mysql/config/JpaConfiguration.java
@@ -21,6 +21,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "com.donniegao.**.dao")
 @EntityScan(basePackages = "com.donniegao.**.model")
 public class JpaConfiguration {
+    /**
+     * 和HibernateTemplate一样，JpaTemplate也会将异常翻译为Spring的数据访问异常，
+     * 而如果改成entityManagerFactory之后，异常就会变成Java SE的异常，比如非法参数，
+     * 非法状态等异常。若要继续使用Spring的异常体系，那么要为JPA的DAO实现类加上@Repository注解，
+     * 然后注册PersistenceExceptionTranslationPostProcessor实例。
+     */
     @Bean
     PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor() {
         return new PersistenceExceptionTranslationPostProcessor();
