55c2f64e766e7daaf5aff91a72b3b40a62db25b6
codegeekgao <codegeek_mrgao@163.com>
2018-04-08 23:27:33 +0800

    提交依赖

---
+++ mongodb/src/main/java/com/codegeekgao/MongodbApplication.java
@@ -0,0 +1,15 @@
+package com.codegeekgao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoxinpeng
+ * @version Id: MongodbApplication.java, v 0.1 2018/4/8 下午11:22 gaoxinpeng Exp $$
+ */
+@SpringBootApplication
+public class MongodbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbApplication.class,args);
+    }
+}
